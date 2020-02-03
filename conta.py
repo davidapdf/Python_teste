f6cddb42f7ae670ae2e743725dd6a7519f8916f4
David Alexandre <david.paula.dias@everis.com>
2020-02-03 17:58:23 -0300

    Atualizacao

--- conta.py
+++ conta.py
@@ -1,16 +1,59 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @saldo.setter
+    def saldo(self, saldo):
+        self.__saldo = saldo
+    @titular.setter
+    def titular(self, titular):
+        self.__titular = titular
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    def __pode_sacar(self, valor):
+        valor_disponivel = self.saldo + self.limite
+        return valor <= valor_disponivel
 
     def extrato(self):
-        print("Saldo {} Titular {}".format(self.saldo, self.titular))
+        print("Saldo {} Titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     def saca(self, valor):
-        self.saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("Valor insuficiente {}".format(valor))
+
+    def trasferir(self,valor,conta):
+        self.saca(valor)
+        conta.deposita(valor)
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+
+
 
 
