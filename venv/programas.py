25680172a119669de8167eae34b58f24253af60e
David Alexandre <david.paula.dias@everis.com>
2020-02-03 19:16:47 -0300

    Atualizacao

--- venv/programas.py
+++ venv/programas.py
@@ -3,6 +3,9 @@ class Programa:
         self._nome = nome.title()
         self.ano = ano
         self._likes = 0
+
+        def __str__(self):
+            return f'Nome: {self.nome} Likes: {self.likes}'
     @property
     def likes(self):
         return self._likes
@@ -16,10 +19,19 @@ class Filme(Programa):
     def __init__(self,nome, ano, duracao):
         super().__init__(nome,ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
 class Serie(Programa):
     def __init__(self,nome, ano, temporadas):
         super().__init__(nome,ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+
+
 vingadores = Filme("Vingadores",2019,160)
 atlanta = Serie("atlanta",2015,10)
 vingadores.dar_likes()
@@ -29,5 +41,7 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - likes: {vingadores.likes}')
-print(f"Nome: {atlanta.nome} Likes: {atlanta.likes}")
+listinha = {vingadores,atlanta}
+
+for programa in listinha:
+    print(programa)
